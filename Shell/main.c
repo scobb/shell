9f6e0ccee7b3dad4143e1c0511b786e0fc7b9c44
Steve Cobb <scobb1@utexas.edu>
2015-08-31 17:48:29 -0500

    Token parsing done.

--- Shell/main.c
+++ Shell/main.c
@@ -1,61 +1,119 @@
+// standard includes
 #include <stdlib.h>
 #include <stdio.h>
-void shell_loop();
-char* shell_read_line();
+#include <signal.h>
+#include <string.h>
 
+// definitions
+#define FINISHED 1
+#define SHELL_RL_BUFSIZE 2000
+#define SHELL_TOK_BUFSIZE 64
+#define SHELL_TOK_DELIM " "
+
+// forward declarations
+void shell_loop();
+char* shell_read_line(int* status);
+void handle_signal();
+char** shell_split_line(char* line);
+
+// main
 int main(int argc, char **argv) {
 
-	shell_loop();
+    shell_loop();
 
-	return 0;
+    return 0;
 }
 
+// allocation check
+void allocation_check(void* p) {
+    if (!p) {
+        fprintf(stderr, "Shell: allocation error\n");
+        exit(1);
+    }
+}
+
+// signal handler
+void handle_signal(int signum) {
+    if (signum == SIGINT) {
+        printf("GOT SIGINT\n");
+        exit(0);
+    }
+}
+
+// primary functionality -- shell loop
 void shell_loop() {
-	char* line;
-	char** args;
-	int status;
-	do {
-		printf("> ");
-		line = shell_read_line();
-		printf("%s\n", line);
-		/*args = shell_split_line(line);
-		status = shell_execute(args);
-		free(line);
-		free(args);*/
+    // local vars for parsing
+    char* line;
+    char** args;
 
-	} while (status);
+    // we'll go until we find eof.
+    int status = 0;
+
+    // register handler
+    signal(SIGINT, handle_signal);
+
+    // loop forever
+    while (1){
+        printf("> ");
+        line = shell_read_line(&status);
+        if (status == FINISHED)
+            break;
+        printf("%s\n", line);
+        args = shell_split_line(line);
+        //status = shell_execute(args);
+        free(line);
+        free(args);
+
+    }
 }
 
-#define SHELL_RL_BUFSIZE 1024
-char* shell_read_line() {
-	int bufsize = SHELL_RL_BUFSIZE;
-	int position = 0;
-	char* buf = (char*)malloc(sizeof(char) * bufsize);
-	int c;
+char* shell_read_line(int* status) {
+    int bufsize = SHELL_RL_BUFSIZE;
+    int position = 0;
+    char* buf = (char*)malloc(sizeof(char) * bufsize);
+    int c;
+    allocation_check(buf); 
 
-	if (!buf) {
-		fprintf(stderr, "Shell: allocation error\n");
-		exit(1);
-	}
+    while (1) {
+        c = getchar();
 
-	while (1) {
-		c = getchar();
+        if (c == EOF) {
+            // break the main loop
+            *status = FINISHED;
+            buf[position] = 0;
+            printf("status is %d\n", *status);
+            return buf;
+        } else if (c == '\n') {
+            // finish the command
+            buf[position] = 0;
+            return buf;
+        }
+        else {
+            buf[position++] = c;
+        }
+    }
+}
 
-		if (c == EOF || c == '\n') {
-			buf[position] = 0;
-			return buf;
-		}
-		else {
-			buf[position++] = c;
-		}
+char** shell_split_line(char* line){
+    int bufsize = SHELL_TOK_BUFSIZE;
+    int position = 0;
+    char** tokens = (char**)malloc(bufsize * sizeof(char*));
+    char* token;
+    allocation_check(tokens);
 
-		if (position >= bufsize) {
-			bufsize += SHELL_RL_BUFSIZE;
-			buf = (char*)realloc(buf, bufsize);
-			if (!buf) {
-				fprintf(stderr, "Shell: allocation error\n");
-				exit(1);
-			}
-		}
-	}
+    // get first token
+    token = strtok(line, SHELL_TOK_DELIM);
+    while (token != NULL) {
+        printf("Token: %s\n", token);
+        tokens[position++] = token;
+
+        // too many tokens?
+        if (position >= bufsize) {
+            bufsize += SHELL_TOK_BUFSIZE;
+            tokens = (char**)realloc(tokens, bufsize * sizeof(char*));
+            allocation_check(tokens);
+        }
+        // get next token
+        token = strtok(NULL, SHELL_TOK_DELIM);
+    }
 }
